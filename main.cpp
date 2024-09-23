162209f24cf1b194edc67d7c62bb627f8a467cc4
seidlmaxi <max.seidl0914@gmail.com>
2024-09-23 20:51:52 +0200

    Hibák kommentelése

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // NELEMENTS helyett N_ELEMENTS nek kéne a változót nevezni
+    std::cout << '1-100 ertekek duplazasa' // ' ' helyett " " közé kellene rakni az std::cout << utáni szöveget, pontosvesszõ hiányzik a végén
+    for (int i = 0;) // Hibás a ciklus így nem fog jól lefutni, (i <N_ELEMENTS ) lenne helyes
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // (i <N_ELEMENTS ) lenne helyes az i inicializálásánál
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // nem irja ki az értéket és pontosvesszõ sincs
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; // nincs inicializálva a változó
+    for (int i = 0; i < N_ELEMENTS, i++) // ';'-nek kell lennie a for ciklus feltételeit elválasztó karakternek ,nem ','
     {
-        atlag += b[i]
+        atlag += b[i] // ';' a sor végén hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
