4b5ed186fba399069173358dc7a375a9d4c56861
seidlmaxi <max.seidl0914@gmail.com>
2024-11-22 11:09:27 +0100

    errors commented in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // Hiba: "NELEMENTS" helyett "N_ELEMENTS" kell
+    std::cout << '1-100 ertekek duplazasa' //Az idézõjelek helyett aposztrófot használ és a pontosvesszõ hiányzik a végérõl
+    for (int i = 0;) //A for ciklus feltételei hiányosak (nem megfelelõ a feltétel és a léptetés)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)//A ciklus feltétele így mindig hamis lesz mert az "i" nem változhat
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //hiányzik a pontosvesszõ és magát az értéket ez nem írja ki
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //Nincs inicializalva az "atlag" valtozo
+    for (int i = 0; i < N_ELEMENTS, i++) //a for ciklus feltételeinek elválasztására pontosvesszõt kellene alkalmazni nem sima vesszõt
     {
-        atlag += b[i]
+        atlag += b[i] //Hiányzik a pontosvesszõ a végérõl
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
